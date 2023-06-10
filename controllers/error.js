884f94f4a965577c904d3e702c5659e58d662a67
Habibulloh <darvesh1304@gmail.com>
2023-06-10 10:08:03 +0500

    changed token and del errors.js getActiveMember.js

--- controllers/error.js
+++
@@ -1,4 +0,0 @@
-export const errorSave = (errText) => {
-  const time = new Date()
-  
-}
--- controllers/start.js
+++ controllers/start.js
@@ -5,7 +5,6 @@ const userSchema = require('../models/userSchema')
 const { isMemberInAll } = require('./isMember')
 const { hasAccount, hasFullInfo, stepSwitcher } = require('./hasAccount')
 const UserModel = require("../models/userSchema");
-const { getActiveMembers } = require('./getActiveMembers')
 
 bot.start(async (ctx) => {
   try {
@@ -32,59 +31,6 @@ bot.start(async (ctx) => {
     console.log(error);
   }
 });
-
-
-bot.command('top', async (ctx) => {
-  getActiveMembers(ctx)
-})
-
-let addedUserCount = 0;
-
-bot.on('new_chat_members', async (ctx) => {
-  const from = ctx.message.from;
-  let currentUser = await UserModel.findOne({ id: from.id });
-
-  let memberCount = ctx.message.new_chat_members.length;
-  addedUserCount += memberCount;
-
-  if (currentUser) {
-    currentUser.addedUserCount = addedUserCount;
-  } else {
-    // create newuser
-    currentUser = new UserModel({
-      id: from.id,
-      addedUserCount: addedUserCount,
-      username: ctx.from.username,
-      firstname: ctx.from.first_name,
-    });
-  }
-  await currentUser.save();
-});
-
-
-
-// bot.on('new_chat_members', async (ctx) => {
-//   const from = ctx.message.from;
-//   let currentUser = await UserModel.findOne({ id: from.id });
-
-//   let memberCount = ctx.message.new_chat_members.length;
-
-//   if (currentUser) {
-//     currentUser.addedUserCount += memberCount;
-//     console.log(currentUser.addedUserCount)
-//     console.log(memberCount)
-//     await currentUser.save();
-//   } else {
-//     // create new user
-//     currentUser = new UserModel({
-//       id: from.id,
-//       addedUserCount: memberCount,
-//       username: ctx.from.username,
-//       firstname: ctx.from.first_name,
-//     });
-//     await currentUser.save();
-//   }
-// })
 
 bot.on('callback_query', async (ctx) => {
   try {
